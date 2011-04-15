1ee7f4aa1202d5b5560cd7fd84e3f19b7f93f7b1
Philipp Wassibauer <phil@maptales.com>
2011-04-15 19:23:51 +0200

    latest_notices added to context_processors

--- notification/context_processors.py
+++ notification/context_processors.py
@@ -4,6 +4,7 @@ def notification(request):
     if request.user.is_authenticated():
         return {
             'notice_unseen_count': Notice.objects.unseen_count_for(request.user, on_site=True),
+            'latest_notices': Notice.objects.notices_for(request.user)[0:8]
         }
     else:
         return {}
